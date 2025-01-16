8c82d6647f9f3e2e14b2048628bc8568fd6c8163
Jakub Bröckl <brockljakub@gmail.com>
2025-01-16 19:38:29 +0100

    Add: Extinguisher Entity

--- src/main/java/cz/uhk/kppro/repository/DriverRepository.java
+++
@@ -1,8 +0,0 @@
-package cz.uhk.kppro.repository;
-
-import cz.uhk.kppro.model.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-
-}
